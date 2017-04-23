13f1b298390d7ae9604b09f18b47fe4278678fde
Lucas <Lucas@DESKTOP-VRE9Q57>
2017-04-23 02:53:58 -0300

    Alterações em ControleLogicoRota

--- src/java/Controle/jsonServleti.java
+++ src/java/Controle/jsonServleti.java
@@ -1,10 +1,12 @@
-
 package Controle;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dao.DaoRelatorio;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -21,37 +23,85 @@ public class jsonServleti extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     DaoRelatorio acessohibernaterelatorio;
-    
+
     public jsonServleti() {
 
         acessohibernaterelatorio = new DaoRelatorio();
 
     }
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       
-        List <Pedido> ListaPedidosAbertos;
+
+        List<Pedido> ListaPedidosAbertos;
         ListaPedidosAbertos = (List<Pedido>) acessohibernaterelatorio.PedidosAbertos(Pedido.class);
-        
+
         List<String> ListaStatus = new ArrayList<>();
-        
+
         ListaPedidosAbertos.forEach(l -> ListaStatus.add(l.getStatus()));
         Map<String, Long> counts = ListaStatus.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
-        
+
+        List<jsonServleti.pedidoStatus> ListaQuantidade = new ArrayList<jsonServleti.pedidoStatus>();
+        jsonServleti.pedidoStatus pedidos_status;
+
+        for (Map.Entry<String, Long> count : counts.entrySet()) {
+            
+            pedidos_status = new pedidoStatus();
+            pedidos_status.setStatus(count.getKey());
+            pedidos_status.setQuantidade(count.getValue());
+            ListaQuantidade.add(pedidos_status);
+        }
+
+        try (BufferedWriter buff_writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\Lucas\\Google Drive\\NetBeansProjects\\Projeto_SGER2203\\web\\JSON\\ListaQuantidade.json", false), "UTF-8"))) {
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(ListaQuantidade, buff_writer);
+        }
+
         String json;
         Gson gson = new Gson();
         gson = new GsonBuilder().create();
-        json = gson.toJson(counts);
+        json = gson.toJson(ListaQuantidade);
 
         response.setContentType("application/json");
         response.getWriter().write(json);
     }
-    
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-   
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 
-    
+    static class pedidoStatus {
+
+        private String status;
+        private long quantidade;
+
+        /**
+         * @return the status
+         */
+        public String getStatus() {
+            return status;
+        }
+
+        /**
+         * @param status the status to set
+         */
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        /**
+         * @return the quantidade
+         */
+        public long getQuantidade() {
+            return quantidade;
+        }
+
+        /**
+         * @param quantidade the quantidade to set
+         */
+        public void setQuantidade(long quantidade) {
+            this.quantidade = quantidade;
+        }
+    }
 }
--- web/JSProjeto/relatoriosi.js
+++ web/JSProjeto/relatoriosi.js
@@ -1,73 +1,73 @@
 google.charts.load('current', {packages: ['corechart']});
 
-document.getElementById("Buscar1").onclick = drawChart2;
-google.charts.setOnLoadCallback(drawChart1);
+//google.charts.setOnLoadCallback(drawChart1);
 google.charts.setOnLoadCallback(drawChart2);
-
-function drawChart1() {
-    
-    var data = new google.visualization.arrayToDataTable([
-        ['Status', 'Quantidade'],
-        ['Entregue', 27],
-        ['Nao Entregue', 8],
-        ['Cancelado', 3], 
-    ]);
-
-    var options = {
-        title: 'Pedidos Fechados',
-        titleTextStyle:{fontSize: 32, bold:true},
-        legend:'top',
-        pieSliceText:'value',
-        pieSliceText:'percentage'
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('chart1'));
-    chart.draw(data, options);
-}
-
-
-function drawChart2() {
-    
-    var grafico_json = pedidosabertosJSON ();
-    var grafico_formatado = [];
-    
-//    $.each(grafico_json, function (key, value) {
-//        grafico_formatado.push([obj.key, obj.value]);
-//    });
-    
-    
-    
+//
+//function drawChart1() {
+//    
 //    var data = new google.visualization.arrayToDataTable([
 //        ['Status', 'Quantidade'],
-//        ['Aguardando Entrega', 7],
-//        ['Entrega em Andamento', 9],
+//        ['Entregue', 27],
+//        ['Nao Entregue', 8],
+//        ['Cancelado', 3], 
 //    ]);
 //
 //    var options = {
-//        title: 'Pedidos Abertos',
+//        title: 'Pedidos Fechados',
 //        titleTextStyle:{fontSize: 32, bold:true},
 //        legend:'top',
+//        pieSliceText:'value',
+//        pieSliceText:'percentage'
 //    };
-
-//    var chart = new google.visualization.PieChart(document.getElementById('chart2'));
+//
+//    var chart = new google.visualization.PieChart(document.getElementById('chart1'));
 //    chart.draw(data, options);
+//}
+
+document.getElementById("Buscar1").onclick = drawChart2;
+
+function drawChart2() {
+
+    var grafico_json = pedidosabertosJSON();
+    var grafico_formatado = [];
+
+    $.each(grafico_json, function (i, obj) {
+        grafico_formatado.push([obj.status, obj.quantidade]);
+    });
+
+    console.log(grafico_formatado);
+
+    var data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Status');
+    data.addColumn('number', 'Quantidade');
+    data.addRows(grafico_formatado);
+
+
+    var options = {
+        title: 'Pedidos Abertos',
+        titleTextStyle: {fontSize: 32, bold: true},
+        legend: 'top',
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('chart2'));
+    chart.draw(data, options);
 }
-//    google.charts.setOnLoadCallback(drawChart);
-    
-    function pedidosabertosJSON() { 
-        
-        var arrayJSON = [] ;
-        $.ajax({
-            url: 'jsonServleti',
-            type: 'get',
-            dataType: 'json',
-            async: false,
-            success: function (json) {
-                $.each(json, function(index, pedido){
-                    arrayJSON(index) = pedido;
-                    console.log(arrayJSON[index]);
-                });
-            }
-        });
-        return arrayJSON;
-    }
+google.charts.setOnLoadCallback(drawChart2);
+
+function pedidosabertosJSON() {
+
+    var arrayJSON = [];
+    $.ajax({
+        url: 'jsonServleti',
+        type: 'get',
+        dataType: 'json',
+        async: false,
+        success: function (json) {
+            $.each(json, function (index, pedido) {
+                arrayJSON[index] = pedido;
+//                console.log(arrayJSON[index]);
+            });
+        }
+    });
+    return arrayJSON;
+}
